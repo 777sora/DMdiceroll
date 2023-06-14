1ee85ed6aed872cb04eca7bb95037dfe303b81e7
児山　空 <k.sora0928@gmail.com>
2023-06-14 19:32:45 +0900

    Dice game: inital revision

---
+++ diceroll.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+class diceroll{
+
+public static void main(String[] a){
+
+Random rand = new Random();
+
+
+int d1 = rand.nextInt(6)+1;
+int d2 = rand.nextInt(6)+1;
+int sum = d1 + d2;
+
+
+System.out.println("Rolling dice...");
+System.out.println("Dice 1: " + d1);
+System.out.println("Dice 2: " + d2);
+System.out.println("Total value: " + sum);
+
+
+
+
+}
+}
